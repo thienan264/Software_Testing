a02cfa19af6464265c6f948eb5daf78f5c076b7c
ThienAn <phamanmap2004@mail.com>
2026-02-03 15:48:16 +0700

    Lab6

---
+++ Lab6/src/main/java/fploy/JUnitMessage.java
@@ -0,0 +1,20 @@
+package fploy;
+
+public class JUnitMessage {
+    private String message;
+
+    public JUnitMessage(String message) {
+        this.message = message;
+    }
+
+    public void printMessage() {
+        System.out.println(message);
+        int divide = 1 / 0; 
+    }
+
+    public String printHiMessage() {
+        message = "Hi!" + message;
+        System.out.println(message);
+        return message;
+    }
+}
---
+++ Lab6/src/test/java/fploy/bai1/ArithmeticTest.java
@@ -0,0 +1,24 @@
+package fploy.bai1;
+
+import static org.junit.Assert.assertEquals;
+import fploy.JUnitMessage;
+import org.junit.Test;
+
+public class ArithmeticTest {
+
+    public String message = "Fpoly exception";
+    JUnitMessage junitMessage = new JUnitMessage(message);
+
+    @Test(expected = ArithmeticException.class)
+    public void testJUnitMessage() {
+        System.out.println("Fpoly JUnit Message exception is printing ");
+        junitMessage.printMessage();
+    }
+
+    @Test
+    public void testJUnitHiMessage() {
+        message = "Hi!" + message;
+        System.out.println("Fpoly JUnit Message is printing ");
+        assertEquals(message, junitMessage.printHiMessage());
+    }
+}
